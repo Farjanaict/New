09d34e8ad348a68d82b15b2a0096cdbee1c9ca47
Farjanaict <IT23003@mbstu.ac.bd>
2024-11-18 08:25:19 +0600

    Add files via upload

---
+++ Fivetimeprint.java
@@ -0,0 +1,12 @@
+// five times print Welcome to Java
+public class Fivetimeprint
+{
+public static void main(String[] args)
+{
+int i;
+for(i=1;i<=5;i++)
+{
+System.out.println("Welcome to Java");
+}
+}
+}
