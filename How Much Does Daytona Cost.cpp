2209c5a2af1c0d663ed9d9612789e0f7b08104ee
Farjanaict <IT23003@mbstu.ac.bd>
2025-06-11 22:43:08 +0600

    Add files via upload

---
+++ How Much Does Daytona Cost.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+     {
+      int t;
+      cin>>t;
+     while(t--)
+     {
+        int n,k,c=0;
+        cin>>n>>k;
+        int a[n];
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+         for(int i=0;i<n;i++)
+         {
+             if(a[i]==k)
+                c++;
+         }
+         if(c>0)
+            cout<<"YES"<<endl;
+         else
+            cout<<"NO"<<endl;
+
+     }
+    return 0;
+}
+
+
+
